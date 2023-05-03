3e4bb0c48f28da56254d2e1c27bb82331fed9d53
Hussain <hussain@hashmac.com>
2023-05-03 22:16:16 +0500

    Add Sound and Vibration

--- app/src/main/java/com/hashmac/snapshotscramble/ui/HelpActivity.java
+++ app/src/main/java/com/hashmac/snapshotscramble/ui/HelpActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hashmac.snapshotscramble.databinding.ActivityHelpBinding;
+import com.hashmac.snapshotscramble.utils.Config;
 
 public class HelpActivity extends BaseActivity {
     ActivityHelpBinding binding;
@@ -18,5 +19,17 @@ public class HelpActivity extends BaseActivity {
     }
     private void init() {
         binding.backBtnImage.setOnClickListener(view -> finish());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Config.checkMusic(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Config.stopMusic(this);
     }
 }
