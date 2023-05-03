fe0d6eb217c422e984a518f0eab7320a337eae3e
Hussain <hussain@hashmac.com>
2023-05-03 22:00:15 +0500

    Update UI and Design of App revert All Lost commit

---
+++ openCV/build/intermediates/cxx/Debug/1n2f24t2/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Hussain\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Hussain\\AndroidStudioProjects\\SnapshotScramble\\openCV\\.cxx\\Debug\\1n2f24t2\\x86" ^
+  opencv_jni_shared
